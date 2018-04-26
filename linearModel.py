5f6a768952ba07a5c7d0fb576ee252da554197c4
Saddat Hasan <dr.zola@hydra-machine.local>
2018-04-26 18:31:08 +0600

    linear model

---
+++ linearModel.py
@@ -0,0 +1,31 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn
+
+# Load the data
+oecd_bli = pd.read_csv("oecd_bli_2015.csv", thosands=',')
+gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t',
+                             encoding='latin1', na_value='n/a')
+
+# prepare the data
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+X = np.c_[country_stats["GDP per capita"]]
+Y = np.c_[country_stats["Life satisfaction"]]
+
+# visualize the data
+country_stats.plot(kind='scatter', x="GDP per capita", y='Life satisfaction')
+plt.show()
+
+
+# Select a linear model
+lin_reg_model = sklearn.linear_model.LinearRegression()
+
+# Train the model
+lin_reg_model.fit(X, y)
+
+
+# Make a prediction for Cyprus
+X_new = [[22587]]  # Cyprus' GDP per capita
+print(lin_reg_model.predict(X_new))  # outputs [[ 5.96242338]]
